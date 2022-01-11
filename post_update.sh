8e4879792c2446c895757c84efa03a32d979b55a
CNCcamon1 <cnccamon@gmail.com>
2022-01-11 17:35:35 -0500

    Update post_update.sh

--- post_update.sh
+++ post_update.sh
@@ -10,6 +10,7 @@ fi
 
 # Clone source from official MineOS repository
 cd /usr/local/games
+ECHO "TEST"
 git clone https://github.com/hexparrot/mineos-node --quiet --depth 1 minecraft
 if [ $? -ne 0 ] ; then
   echo "ERROR: Failed to get source from MineOS Repository"
